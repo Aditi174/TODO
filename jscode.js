09bea0f4dc97e78d5e4e4b907c2b468c2996e9f9
Aditi174 <070801ap@gmail.com>
2023-10-29 16:51:46 +0530

    add the delete icon

--- jscode.js
+++ jscode.js
@@ -3,30 +3,47 @@ const whole = document.getElementById("item");
 const add_button = document.getElementById("button");
 let array = JSON.parse(localStorage.getItem("keys")) || [];
 
-add_button.addEventListener("click",() => {
+add_button.addEventListener("click", () => {
     const user_input = input.value;
     array.push(user_input);
 
-    localStorage.setItem("keys",JSON.stringify(array));
+    localStorage.setItem("keys", JSON.stringify(array));
 
-    input.value = ""
+    input.value = "";
 
-    display()
-})
+    display();
+});
 
-function display(){
-
+function display() {
     whole.innerHTML = "";
 
-    for(const i of array){
+    for (const i of array) {
         const list = document.createElement("li");
         list.textContent = i;
 
-        whole.appendChild(list);
+        let icon = document.createElement("i");
+        icon.className = "fa-solid fa-trash";
+        list.appendChild(icon);
 
+        icon.addEventListener('click', () => {
+            deleteItem(i);
+        });
+
+        whole.appendChild(list);
     }
 }
 
-display()
+function deleteItem(i) {
+    // Remove the specified item from the array  and item is a parameter that represents each individual element in the array as the filter() method iterates through the array.
+    array = array.filter(item => item !== i) ;
+
+    
+    localStorage.setItem("keys", JSON.stringify(array));
+
+    display();
+}
+
+display();
+
 
 
